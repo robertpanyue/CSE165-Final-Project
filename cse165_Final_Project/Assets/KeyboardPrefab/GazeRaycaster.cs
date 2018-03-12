11609e5873316537f35509d7f0cb028f51236050
Yue Pan <y4pan@UCSD.EDU>
2018-03-11 20:45:47 -0700

    initial project

---
+++ cse165_Final_Project/Assets/KeyboardPrefab/GazeRaycaster.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class GazeRaycaster : MonoBehaviour {
+
+    public float loadingTime;
+    public Image circle;
+    private string lastTargetName = "";
+    public GameObject StartMenu;
+    public GameObject KeyText;
+    public Text nameText;
+    public GameObject Gazepoint;
+    public GameObject Keyboard;
+
+    void FixedUpdate() {
+        RaycastHit hit;
+        
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Debug.Log(KeyText.GetComponent<Text>().text);
+        nameText.text = KeyText.GetComponent<Text>().text;
+
+        if (Physics.Raycast(transform.position, fwd, out hit)) {
+
+            if (hit.transform.tag == "VRGazeInteractable") {
+                if (lastTargetName == hit.transform.name) {
+                    return;
+                }
+
+                if (lastTargetName == "") {
+                    lastTargetName = hit.transform.name;
+                }
+
+                if (hit.transform.name != lastTargetName) {
+                    circle.fillAmount = 0f;
+                    lastTargetName = hit.transform.name;
+                    return;
+                }
+
+                StartCoroutine(FillCircle(hit.transform));
+                return;
+            } else if (hit.transform.tag == "Start") {
+                lastTargetName = hit.transform.name;
+                StartCoroutine(FillCircle(hit.transform));
+            } else if (hit.transform.tag == "Quit") {
+                lastTargetName = hit.transform.name;
+                StartCoroutine(FillCircle(hit.transform));
+            }
+
+            else {
+                circle.fillAmount = 0f;
+                lastTargetName = "";
+            }
+        } else {
+            circle.fillAmount = 0f;
+            lastTargetName = "";
+        }
+    }
+
+    private IEnumerator FillCircle(Transform target) {
+        float timer = 0f;
+        circle.fillAmount = 0f;
+        while (timer < loadingTime) {
+            if (target.name != lastTargetName) {
+                yield break;
+            }
+            timer += Time.deltaTime;
+            circle.fillAmount = timer / loadingTime;
+            yield return null;
+        }
+        circle.fillAmount = 1f;
+        if (target.GetComponent<Button>()) {
+            target.GetComponent<Button>().onClick.Invoke();
+        }
+        if (target.tag == "Start") { 
+            StartMenu.SetActive(false);
+            Gazepoint.SetActive(false);
+            Keyboard.SetActive(false);
+        }
+
+        if (target.tag == "Quit")
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
+        circle.fillAmount = 0f;
+        lastTargetName = "";
+    }
+
+
+
+}
---
+++ cse165_Final_Project/Assets/KeyboardPrefab/KeyboardManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class KeyboardManager : MonoBehaviour {
+
+    public bool isUppercase = false;
+    public int maxInputLength;
+    public Text inputText;
+    public Transform characters;
+
+
+ 
+
+    private string Input {
+        get { return inputText.text;  }
+        set { inputText.text = value;  }
+    }
+
+    private Dictionary<GameObject, Text> keysDictionary = new Dictionary<GameObject, Text>();
+
+    private bool capslockFlag;
+
+    private void Awake() {
+            
+        for (int i = 0; i < characters.childCount; i++) {
+            GameObject key = characters.GetChild(i).gameObject;
+            Text text = key.GetComponentInChildren<Text>();
+            keysDictionary.Add(key, text);
+
+            key.GetComponent<Button>().onClick.AddListener(() => {
+                GenerateInput(text.text);
+            });
+
+        }
+        capslockFlag = isUppercase;
+        CapsLock();
+    }
+
+    public void Backspace() {
+        if (Input.Length > 0) {
+            Input = Input.Remove(Input.Length - 1);
+        } else {
+            return;
+        }
+    }
+
+    public void Clear() {
+        Input = "";
+    }
+
+    public void CapsLock() {
+        if (capslockFlag) {
+            foreach (var pair in keysDictionary) {
+                pair.Value.text = ToUpperCase(pair.Value.text);
+            }
+        } else {
+            foreach (var pair in keysDictionary) {
+                pair.Value.text = ToLowerCase(pair.Value.text);
+            }
+        }
+        capslockFlag = !capslockFlag;
+    }
+        
+    public void GenerateInput(string s) {
+        if (Input.Length > maxInputLength) { return; }
+        Input += s;
+    }
+
+    private string ToLowerCase(string s) {
+        return s.ToLower();
+    }
+
+    private string ToUpperCase(string s) {
+        return s.ToUpper();
+    }
+
+}
