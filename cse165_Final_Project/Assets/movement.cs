a21e068dea3fe7748ae07c2403eaefe29e575ef8
Chaolei Hua <c8hua@UCSD.EDU>
2018-03-18 23:44:48 -0700

    generate box

--- cse165_Final_Project/Assets/movement.cs
+++ cse165_Final_Project/Assets/movement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour {
 
@@ -26,12 +27,11 @@ public class movement : MonoBehaviour {
     private int score;
     public GameObject scorePanel1;
     private GameObject nextBlock;
-    private GameObject currBlock;
+    public GameObject currBlock;
 
     // Use this for initialization
     void Start() {
         controller = GetComponent<CharacterController>();
-
     }
 
     // Update is called once per frame
@@ -43,6 +43,7 @@ public class movement : MonoBehaviour {
             {
                 preHandPosition = new Vector3(Mathf.Min(leftHand.transform.position.x, rightHand.transform.position.x), Mathf.Min(leftHand.transform.position.y, rightHand.transform.position.y), Mathf.Min(leftHand.transform.position.z, rightHand.transform.position.z));
                 Debug.Log("Down");
+
             }
             verticalVelocity = -gravity * Time.deltaTime;
             xdiff = 0;
@@ -98,7 +99,7 @@ public class movement : MonoBehaviour {
     void SpawnBlock()
     {
         int index = Random.Range(0, blockList.Count-1);
-        int dir = Random.Range(2,2);
+        int dir = Random.Range(0,4);
         float dist = Random.Range(10f, 12.0f);
 
         
@@ -106,22 +107,22 @@ public class movement : MonoBehaviour {
         {
             //right
             case 0:
-                GameObject nextBlock1 = Instantiate(blockList[index], new Vector3(transform.position.x +3.8f + dist, 71.7f, transform.position.z+3f), Quaternion.identity);
-                nextBlock1.transform.Rotate(0,180f,0);
+                nextBlock = Instantiate(blockList[index], new Vector3(transform.position.x +3.8f + dist, 71.7f, transform.position.z+3f), Quaternion.identity);
+                nextBlock.transform.Rotate(0,180f,0);
                 break;
             //left
             case 1:
-                GameObject nextBlock2 = Instantiate(blockList[index], new Vector3(transform.position.x + 3.8f - dist, 71.7f, transform.position.z - 3f), Quaternion.identity);
+                nextBlock= Instantiate(blockList[index], new Vector3(transform.position.x + 3.8f - dist, 71.7f, transform.position.z - 3f), Quaternion.identity);
                 break;
             //forward
             case 2:
-                GameObject nextBlock3 = Instantiate(blockList[index], new Vector3(transform.position.x - 2f, 71.7f, transform.position.z - 3f + dist), Quaternion.identity);
-                nextBlock3.transform.Rotate(0, 90f, 0);
+                nextBlock = Instantiate(blockList[index], new Vector3(transform.position.x - 2f, 71.7f, transform.position.z - 3f + dist), Quaternion.identity);
+                nextBlock.transform.Rotate(0, 90f, 0);
                 break;
             //back
             case 3:
-                GameObject nextBlock4 = Instantiate(blockList[index], new Vector3(transform.position.x + 3.8f, 71.7f, transform.position.z - 3f - dist), Quaternion.identity);
-                nextBlock4.transform.Rotate(0, -90f, 0);
+                nextBlock = Instantiate(blockList[index], new Vector3(transform.position.x + 3.8f, 71.7f, transform.position.z - 3f - dist), Quaternion.identity);
+                nextBlock.transform.Rotate(0, -90f, 0);
                 break;
         }
         
@@ -130,15 +131,37 @@ public class movement : MonoBehaviour {
 
 
 
+    /*
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "startcube")
         {
             score += 10;
             scorePanel1.GetComponent<Text>().text = score.ToString();
-            Debug.Log(score);
+            Debug.Log("++++++++++++++++++++++++++++++++++++++++++++++++++");
 
         }
+    }
+    */
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "ground")
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (hit.transform.tag != currBlock.transform.tag)
+        {
+            score += 10;
+            scorePanel1.GetComponent<Text>().text = score.ToString();
+            Debug.Log("++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Destroy(currBlock);
+            currBlock = nextBlock;
+            spawn = true;
+            count = 1;
+        }
+
     }
 
 
